695eece74f2a12c5739da673af8cc7a213f775da
Andrei Poenaru <ap13004@my.bristol.ac.uk>
2020-09-02 14:20:33 +0000

    CloverLeaf: Add TX2 SYCL

--- benchmarking/2020/cloverleaf/tx2-isambard/benchmark.sh
+++ benchmarking/2020/cloverleaf/tx2-isambard/benchmark.sh
@@ -17,6 +17,9 @@ function usage() {
   echo "    cce-10.0"
   echo "    gcc-9.3"
   echo
+  echo "  sycl"
+  echo "    hipsycl-200527-gcc"
+  echo
   echo "The default configuration is '$DEFAULT_MODEL $DEFAULT_COMPILER'."
   echo
 }
@@ -40,70 +43,60 @@ export BENCHMARK_EXE="clover_leaf"
 
 # Set up the environment
 case "$COMPILER" in
-cce-10.0)
-  [ -z "$CRAY_CPU_TARGET" ] && module load craype-arm-thunderx2
-  module swap cce cce/10.0.1
-  MAKE_OPTS='COMPILER=ARM MPI_COMPILER=ftn C_MPI_COMPILER=cc FLAGS_ARM="-em -ra"'
-  ;;
-gcc-9.3)
-  module swap PrgEnv-{cray,gnu}
-  module swap gcc gcc/9.3.0
-  MAKE_OPTS='COMPILER=GNU MPI_COMPILER=cc C_MPI_COMPILER=cc'
-  MAKE_OPTS+=' FLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
-  MAKE_OPTS+=' CFLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
-  ;;
-arm-20.0)
-  module swap PrgEnv-{cray,allinea}
-  module swap allinea allinea/20.0.0.0
-  MAKE_OPTS='COMPILER=GNU MPI_COMPILER=ftn C_MPI_COMPILER=cc'
-  MAKE_OPTS+=' FLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
-  MAKE_OPTS+=' CFLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
-  ;;
-hipsycl-200527-gcc)
-  module purge
-  module load alps PrgEnv-gnu cray-mpich/7.7.12
-  module load hipsycl/gcc/200527
-  ;;
-hipsycl-200527-cce)
-  module purge
-  module load alps PrgEnv-cray cray-mpich/7.7.12
-  module load hipsycl/cce/200527
-  ;;
-hipsycl-200527simd-gcc)
-  module purge
-  module load alps PrgEnv-gnu cray-mpich/7.7.12
-  module load hipsycl/gcc/200527_simd
-  ;;
-*)
-  echo
-  echo "Invalid compiler '$COMPILER'."
-  usage
-  exit 1
-  ;;
+  cce-10.0)
+    [ -z "$CRAY_CPU_TARGET" ] && module load craype-arm-thunderx2
+    module swap cce cce/10.0.1
+    MAKE_OPTS='COMPILER=ARM MPI_COMPILER=ftn C_MPI_COMPILER=cc FLAGS_ARM="-em -ra"'
+    ;;
+  gcc-9.3)
+    module swap PrgEnv-{cray,gnu}
+    module swap gcc gcc/9.3.0
+    MAKE_OPTS='COMPILER=GNU MPI_COMPILER=cc C_MPI_COMPILER=cc'
+    MAKE_OPTS+=' FLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
+    MAKE_OPTS+=' CFLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
+    ;;
+  arm-20.0)
+    module swap PrgEnv-{cray,allinea}
+    module swap allinea allinea/20.0.0.0
+    MAKE_OPTS='COMPILER=GNU MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+    MAKE_OPTS+=' FLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
+    MAKE_OPTS+=' CFLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -mcpu=thunderx2t99 -funroll-loops"'
+    ;;
+  hipsycl-200527-gcc)
+    module swap PrgEnv-{cray,gnu}
+    module load hipsycl/gcc/200527
+    ;;
+  *)
+    echo
+    echo "Invalid compiler '$COMPILER'."
+    usage
+    exit 1
+    ;;
 esac
 
 case "$MODEL" in
-omp)
-  export SRC_DIR="$PWD/CloverLeaf_ref"
-  ;;
-kokkos)
-  KOKKOS_PATH="$PWD/$(fetch_kokkos)"
-  echo "Using KOKKOS_PATH='${KOKKOS_PATH}'"
-  MAKE_OPTS+=" CXX=CC KOKKOS_PATH=${KOKKOS_PATH} ARCH=ARMv8-TX2 DEVICE=OpenMP"
-  [[ "$COMPILER" =~ cce- ]] && MAKE_OPTS+=" KOKKOS_INTERNAL_OPENMP_FLAG=-fopenmp"
-  SRC_DIR="$PWD/cloverleaf_kokkos"
-  ;;
-sycl)
+  omp)
+    export SRC_DIR="$PWD/CloverLeaf_ref"
+    ;;
 
-  HIPSYCL_PATH=$(realpath $(dirname $(which syclcc))/..)
-  echo "Using HIPSYCL_PATH=${HIPSYCL_PATH}"
-  MAKE_OPTS+=" -DHIPSYCL_INSTALL_DIR=${HIPSYCL_PATH} -DSYCL_RUNTIME=HIPSYCL"
-  MAKE_OPTS+=" -DMPI_AS_LIBRARY=ON -DMPI_C_LIB_DIR=${CRAY_MPICH_DIR}/lib -DMPI_C_INCLUDE_DIR=${CRAY_MPICH_DIR}/include -DMPI_C_LIB=mpich"
-  MAKE_OPTS+=" -DCXX_EXTRA_FLAGS=-mtune=native"
+  kokkos)
+    KOKKOS_PATH="$PWD/$(fetch_kokkos)"
+    echo "Using KOKKOS_PATH='${KOKKOS_PATH}'"
+    MAKE_OPTS+=" CXX=CC KOKKOS_PATH=${KOKKOS_PATH} ARCH=ARMv8-TX2 DEVICE=OpenMP"
+    [[ "$COMPILER" =~ cce- ]] && MAKE_OPTS+=" KOKKOS_INTERNAL_OPENMP_FLAG=-fopenmp"
+    SRC_DIR="$PWD/cloverleaf_kokkos"
+    ;;
 
-  export SRC_DIR=$PWD/cloverleaf_sycl
-  export DEVICE_ARGS="--device 1"
-  ;;
+  sycl)
+    HIPSYCL_PATH="$(realpath "$(dirname "$(which syclcc)")"/..)"
+    echo "Using HIPSYCL_PATH=${HIPSYCL_PATH}"
+    MAKE_OPTS+=" -DHIPSYCL_INSTALL_DIR=${HIPSYCL_PATH} -DSYCL_RUNTIME=HIPSYCL"
+    MAKE_OPTS+=" -DMPI_AS_LIBRARY=ON -DMPI_C_LIB_DIR=${CRAY_MPICH_DIR}/lib -DMPI_C_INCLUDE_DIR=${CRAY_MPICH_DIR}/include -DMPI_C_LIB=mpich"
+    MAKE_OPTS+=" -DCXX_EXTRA_FLAGS=-mcpu=native"
+
+    export SRC_DIR="$PWD/cloverleaf_sycl"
+    export DEVICE_ARGS="--device 1"
+    ;;
 esac
 
 # Handle actions
