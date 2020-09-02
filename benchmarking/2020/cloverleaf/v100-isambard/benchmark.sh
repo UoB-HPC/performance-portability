1cc10d14b7108ba408854db64e215e584cfd77d6
Tom Deakin <tom.deakin@bristol.ac.uk>
2020-09-02 11:41:59 +0000

    CloverLeaf V100 SYCL

--- benchmarking/2020/cloverleaf/v100-isambard/benchmark.sh
+++ benchmarking/2020/cloverleaf/v100-isambard/benchmark.sh
@@ -37,15 +37,17 @@ module use /lustre/projects/bristol/modules-power/modulefiles
 module load cuda/10.0
 case "$MODEL" in
 omp-target)
-  module load llvm/trunk
-  module load gcc/8.1.0
-  module load openmpi/3.0.2/gcc8
+  module load openmpi/4.0.0/xl-16 
+ # module load llvm/trunk
+ # module load gcc/8.1.0
+ # module load openmpi/3.0.2/gcc8
   export SRC_DIR="$PWD/CloverLeaf-OpenMP4"
-  export OMPI_CC=clang OMPI_FC=gfortran
-  MAKE_OPTS='-j20 COMPILER=GNU MPI_F90=mpif90 MPI_C=mpicc MPI_LD=mpicc'
+  export OMPI_CC=xlc OMPI_FC=xlf
+  MAKE_OPTS='-j20 COMPILER=XL MPI_F90=mpif90 MPI_C=mpicc MPI_LD=mpicc OMP_XL="-qoffload -qsmp=omp -qthreaded"'
   MAKE_OPTS="$MAKE_OPTS FLAGS_GNU='-O3 -mcpu=power9'"
   MAKE_OPTS="$MAKE_OPTS CFLAGS_GNU='-O3 -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda -Xopenmp-target -march=sm_70' LDLIBS='-lrt -lm -lgfortran -lgomp -lmpi_mpifh'"
   MAKE_OPTS="$MAKE_OPTS LDFLAGS='-O3 -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda -Xopenmp-target -march=sm_70'"
+  BINARY="clover_leaf"
   ;;
 cuda)
   module load cuda/10.0
@@ -68,6 +70,20 @@ acc)
   export SRC_DIR="$PWD/CloverLeaf-OpenACC"
   MAKE_OPTS='COMPILER=PGI C_MPI_COMPILER=mpicc MPI_F90=mpif90 FLAGS_PGI="-O3 -Mpreprocess -fast -acc -ta=tesla:cc70" CFLAGS_PGI="-O3 -ta=tesla:cc70" OMP_PGI=""'
   BINARY="clover_leaf"
+  ;;
+sycl)
+  module load hipsycl/jul-8-20
+  module load gcc/8.1.0
+  module load openmpi/3.0.2/gcc8
+  module load cmake/3.14
+  HIPSYCL_PATH=$(realpath $(dirname $(which syclcc))/..)
+  echo "Using HIPSYCL_PATH=${HIPSYCL_PATH}"
+
+  MAKE_OPTS+=" -DHIPSYCL_INSTALL_DIR=${HIPSYCL_PATH} -DSYCL_RUNTIME=HIPSYCL -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc"
+  MAKE_OPTS+=" -DCXX_EXTRA_FLAGS=-mtune=native -DHIPSYCL_PLATFORM=cuda -DHIPSYCL_GPU_ARCH=sm_70"
+
+  BINARY="clover_leaf"
+  export SRC_DIR=$PWD/cloverleaf_sycl
   ;;
 *)
   echo
