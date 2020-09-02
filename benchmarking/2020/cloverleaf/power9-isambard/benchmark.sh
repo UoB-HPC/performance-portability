adce4811498b36adccd94e2f0c15c15b75973dfb
Tom Deakin <tom.deakin@bristol.ac.uk>
2020-09-02 17:51:28 +0000

    CloverLeaf Power9 Kokkos

--- benchmarking/2020/cloverleaf/power9-isambard/benchmark.sh
+++ benchmarking/2020/cloverleaf/power9-isambard/benchmark.sh
@@ -66,9 +66,6 @@ case "$COMPILER" in
         BINARY="clover_leaf"
         ;;
     xl-16)
-      echo "Compiler fails, try a different one"
-      exit 1
-      module load openmpi/4.0.0/xl-16 
       MAKE_OPTS='COMPILER=XL FLAGS_XL="-O5 -qipa=partition=large -g -qfullpath -Q -qsigtrap -qextname=flush:ideal_gas_kernel_c:viscosity_kernel_c:pdv_kernel_c:revert_kernel_c:accelerate_kernel_c:flux_calc_kernel_c:advec_cell_kernel_c:advec_mom_kernel_c:reset_field_kernel_c:timer_c:unpack_top_bottom_buffers_c:pack_top_bottom_buffers_c:unpack_left_right_buffers_c:pack_left_right_buffers_c:field_summary_kernel_c:update_halo_kernel_c:generate_chunk_kernel_c:initialise_chunk_kernel_c:calc_dt_kernel_c:clover_unpack_message_bottom_c:clover_pack_message_bottom_c:clover_unpack_message_top_c:clover_pack_message_top_c:clover_unpack_message_right_c:clover_pack_message_right_c:clover_unpack_message_left_c:clover_pack_message_left_c -qlistopt -qattr=full -qlist -qreport -qxref=full -qsource -qsuppress=1506-224:1500-036FLAGS_"'
       BINARY="clover_leaf"
       ;;
@@ -86,10 +83,12 @@ case "$MODEL" in
      MAKE_OPTS='COMPILER=PGI FLAGS_PGI="-O3 -Mpreprocess -fast -acc -ta=multicore -tp=pwr9" CFLAGS_PGI="-O3 -ta=multicore -tp=pwr9" OMP_PGI=""'
      ;;
   kokkos)
-     export SRC_DIR="$PWD/cloverleaf_kokkos"
-     module load kokkos/power9/gcc-8.1
-     MAKE_OPTS=''
-     ;;
+    export SRC_DIR="$PWD/cloverleaf_kokkos"
+    KOKKOS_PATH=$(pwd)/$(fetch_kokkos)
+    echo "Using KOKKOS_PATH=${KOKKOS_PATH}"
+    MAKE_OPTS="COMPILER=GNU"
+    MAKE_OPTS+=" KOKKOS_PATH=${KOKKOS_PATH} ARCH=POWER9 DEVICE=OpenMP"
+    ;;
 esac
 
 
