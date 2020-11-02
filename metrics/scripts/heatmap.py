898ac6400a1ef7ff2d638249a8806aefd4bfde88
John Pennycook <john.pennycook@intel.com>
2020-11-02 10:35:01 -0800

    autopep8 heatmap.py

--- metrics/scripts/heatmap.py
+++ metrics/scripts/heatmap.py
@@ -10,12 +10,26 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Argument parsing
 parser = argparse.ArgumentParser()
-parser.add_argument("input",  help="CSV input file")
+parser.add_argument("input", help="CSV input file")
 parser.add_argument("output", help="PDF output file")
-parser.add_argument("--higher-is-better", help="High numbers are better than low number (e.g. when plotting bandwidth)", action="store_true")
-parser.add_argument("--factorize", help="Divide all input results by this number", action="store", type=float, default=1.0)
-parser.add_argument("--percent", help="Input is in percent", action="store_true")
-parser.add_argument("--mean", help="Plot the mean and standard deviation against each column", action="store_true")
+parser.add_argument(
+    "--higher-is-better",
+    help="High numbers are better than low number (e.g. when plotting bandwidth)",
+    action="store_true")
+parser.add_argument(
+    "--factorize",
+    help="Divide all input results by this number",
+    action="store",
+    type=float,
+    default=1.0)
+parser.add_argument(
+    "--percent",
+    help="Input is in percent",
+    action="store_true")
+parser.add_argument(
+    "--mean",
+    help="Plot the mean and standard deviation against each column",
+    action="store_true")
 args = parser.parse_args()
 
 # Open the input .csv file
@@ -28,18 +42,19 @@ headings = data.fieldnames[1:]
 series_key = data.fieldnames[0]
 
 
-series = [] # a row in the input file
+series = []  # a row in the input file
 
-heatmap = [] # empty, to be populated by reading the input file
-labels = [] # labels to display in each heatmap entry
+heatmap = []  # empty, to be populated by reading the input file
+labels = []  # labels to display in each heatmap entry
 
 for result in data:
     def get(name):
         str = result[name]
         try:
             return float(str)
-        except:
+        except BaseException:
             return str
+
     def eff(a, b):
         if isinstance(a, float) and isinstance(b, float):
             return float(100.0 * (a / b))
@@ -57,7 +72,6 @@ for result in data:
     series.append(result[series_key])
     heatmap.append([r if isinstance(r, float) else 0.0 for r in raw])
 
-
     l = []
     for i in range(len(raw)):
         if not isinstance(raw[i], float):
@@ -65,14 +79,14 @@ for result in data:
         else:
             if args.percent:
                 if plt.rcParams['text.usetex']:
-                    l.append('%.0f\\%%' % (raw[i]/args.factorize))
+                    l.append('%.0f\\%%' % (raw[i] / args.factorize))
                 else:
-                    l.append('%.0f%%' % (raw[i]/args.factorize))
+                    l.append('%.0f%%' % (raw[i] / args.factorize))
             else:
-                if raw[i]/args.factorize < 100.0:
-                    l.append('%.1f' % (raw[i]/args.factorize))
+                if raw[i] / args.factorize < 100.0:
+                    l.append('%.1f' % (raw[i] / args.factorize))
                 else:
-                    l.append('%.0f' % (raw[i]/args.factorize))
+                    l.append('%.0f' % (raw[i] / args.factorize))
     labels.append(l)
 
 plt.rcParams.update({
@@ -85,19 +99,27 @@ fig.set_size_inches(4, 3)
 
 # Set color map to match blackbody, growing brighter for higher values
 colors = "gist_heat"
-cmap = plt.cm.get_cmap(colors) if args.higher_is_better else plt.cm.get_cmap(colors+"_r")
+if not args.higher_is_better:
+    colors = colors + "_r"
+cmap = plt.cm.get_cmap(colors)
 x = np.arange(7)
 y = np.arange(11)
-cmesh = plt.pcolormesh(x, y, np.array(heatmap), cmap=cmap, edgecolors='k', vmin=1.0E-6)
+cmesh = plt.pcolormesh(
+    x,
+    y,
+    np.array(heatmap),
+    cmap=cmap,
+    edgecolors='k',
+    vmin=1.0E-6)
 ax.set_yticks(np.arange(len(heatmap)) + 0.5, minor=False)
 ax.set_xticks(np.arange(len(heatmap[0])) + 0.5, minor=False)
 
 ax.set_yticklabels(series)
 for i in range(len(headings)):
-  heading = headings[i].replace('_', '\_')
-  if not plt.rcParams['text.usetex']:
-    heading = heading.replace(r"\%", "%")
-  headings[i] = heading
+    heading = headings[i].replace('_', r'\_')
+    if not plt.rcParams['text.usetex']:
+        heading = heading.replace(r"\%", "%")
+    headings[i] = heading
 ax.set_xticklabels(headings, rotation=45, ha="right", rotation_mode="anchor")
 plt.gca().invert_yaxis()
 
