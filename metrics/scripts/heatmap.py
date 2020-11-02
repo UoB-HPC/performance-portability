ca3e9cab3e7dcd98b0ee31d07916f50f4a27d25d
John Pennycook <john.pennycook@intel.com>
2020-11-02 10:20:16 -0800

    Remove unused code from heatmap.py

--- metrics/scripts/heatmap.py
+++ metrics/scripts/heatmap.py
@@ -54,14 +54,6 @@ for result in data:
     if not any([isinstance(x, float) for x in raw]):
         continue
 
-    # Divide each raw result by the best result.
-    #if result['benchmark'] in ['STREAM','GROMACS']:
-    #    best = max(x for x in raw if isinstance(x, float))
-    #    efficiencies = [eff(x, best) for x in raw]
-    #else:
-    #    best = min(x for x in raw if isinstance(x, float))
-    #    efficiencies = [eff(best, x) for x in raw]
-
     series.append(result[series_key])
     heatmap.append([r if isinstance(r, float) else 0.0 for r in raw])
 
@@ -90,70 +82,32 @@ plt.rcParams.update({
 })
 fig, ax = plt.subplots()
 fig.set_size_inches(4, 3)
-colors = "summer_r"
-colors = "inferno"
+
+# Set color map to match blackbody, growing brighter for higher values
 colors = "gist_heat"
-# Set map so red is best, green is worst
 cmap = plt.cm.get_cmap(colors) if args.higher_is_better else plt.cm.get_cmap(colors+"_r")
-#cmap.set_under('w')
-#plt.pcolor(np.array(heatmap), cmap=cmap, edgecolors='k', vmin=1.0E-6)
 x = np.arange(7)
 y = np.arange(11)
 cmesh = plt.pcolormesh(x, y, np.array(heatmap), cmap=cmap, edgecolors='k', vmin=1.0E-6)
 ax.set_yticks(np.arange(len(heatmap)) + 0.5, minor=False)
 ax.set_xticks(np.arange(len(heatmap[0])) + 0.5, minor=False)
 
-#ax.set_aspect(0.25, adjustable='box')
-#ax.set_yticklabels(series, fontsize='xx-large')
 ax.set_yticklabels(series)
 for i in range(len(headings)):
   heading = headings[i].replace('_', '\_')
   if not plt.rcParams['text.usetex']:
     heading = heading.replace(r"\%", "%")
   headings[i] = heading
-#ax.set_xticklabels(headings, fontsize='xx-large', rotation=45)
 ax.set_xticklabels(headings, rotation=45, ha="right", rotation_mode="anchor")
 plt.gca().invert_yaxis()
 
 # Add colorbar
-#divider = make_axes_locatable(ax)
-#cax = divider.append_axes("right", size="5%", pad=0.05)
-#plt.colorbar(cmesh, cax=cax)
-#plt.colorbar(cmesh, fraction=0.046, pad=0.04)
 plt.colorbar(cmesh)
 
 # Add labels
 for i in range(len(headings)):
     for j in range(len(series)):
-        #plt.text(i + 0.5, j + 0.55, labels[j][i],
         plt.text(i + 0.9, j + 0.5, labels[j][i],
-                 ha='right', va='center', color='#b9c5bf',fontsize='small')#, weight='bold')
-
-# Add caption
-#if args.higher_is_better:
-#    plt.title("Higher is better", fontsize='xx-large')
-#else:
-#    plt.title("Lower is better", fontsize='xx-large')
-
-# Add mean and standard deviation if required
-#if args.mean:
-#  heatmap = np.array(heatmap)
-#  # Calculate mean and standard deviation for each row
-#  means = np.mean(heatmap, axis=1)
-#  stdev = np.std(heatmap, axis=1)
-#  print(means)
-#  print(stdev)
-#
-#  # Plot on graph as extra column
-#  plt.text(len(headings) + 0.5, 0, "Mean",
-#                 ha='center', va='center', color='black', weight='bold', size='xx-large')
-#  plt.text(len(headings) + 1.5, 0, "Std. Dev.",
-#                 ha='center', va='center', color='black', weight='bold', size='xx-large')
-#  for i in range(len(series)):
-#    plt.text(len(headings) + 0.5, i + 0.5, "%.1f" % means[i],
-#                 ha='center', va='center', color='black', weight='bold', size='xx-large')
-#    plt.text(len(headings) + 1.5, i + 0.5, "%.1f" % stdev[i],
-#                 ha='center', va='center', color='black', weight='bold', size='xx-large')
-
+                 ha='right', va='center', color='#b9c5bf', fontsize='small')
 
 plt.savefig(args.output, bbox_inches='tight')
