90f5a478c1603a9d7bc91bd7e062c0d9cb15ab42
Tom Lin <tom91136@gmail.com>
2020-11-24 17:24:33 +0000

    Add Rome/oneAPI+ComputeCpp

--- benchmarking/2021/bude/rome-isambard/benchmark.sh
+++ benchmarking/2021/bude/rome-isambard/benchmark.sh
@@ -14,18 +14,59 @@ setup_env() {
       module swap cce cce/10.0.0
       module swap craype-{broadwell,x86-rome}
       MAKE_OPTS='COMPILER=CLANG CC=cc ARCH=znver2'
+      MAKE_OPTS+=' WGSIZE=512'
       ;;
     gcc-9.3)
       module load gcc/9.3.0
       MAKE_OPTS='COMPILER=GNU ARCH=znver2'
+      MAKE_OPTS+=' WGSIZE=512'
       ;;
     gcc-10.2)
       module load gcc/10.2.0
       MAKE_OPTS='COMPILER=GNU ARCH=znver2'
+      MAKE_OPTS+=' WGSIZE=512'
       ;;
     intel-2019)
       module load intel-parallel-studio-xe/compilers/64/2019u4/19.0.4
       MAKE_OPTS='COMPILER=INTEL ARCH=core-avx2'
+      MAKE_OPTS+=' WGSIZE=512'
+      ;;
+    oneapi-2021.1-beta10)
+      module load gcc/8.2.0
+      loadOneAPI /lustre/projects/bristol/modules/intel/oneapi/setvars.sh
+      module load cmake/3.18.3
+      MAKE_OPTS=" -DSYCL_RUNTIME=DPCPP"
+      MAKE_OPTS+=" -DNUM_TD_PER_THREAD=2"
+      MAKE_OPTS+=" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++"
+      MAKE_OPTS+=" -DCXX_EXTRA_FLAGS=--gcc-toolchain=/cm/local/apps/gcc/8.2.0"
+      ;;
+    hipsycl-46bc9bd)
+      # FIXME 46bc9bd is the head of the stable branch and it still can't handle local_ptr overloads
+      # dev branch compiles but it's fairly volatile there
+      module load hipsycl/46bc9bd
+      HIPSYCL_PATH="$(realpath "$(dirname "$(which syclcc)")"/..)"
+      module load cmake/3.18.3 gcc/8.2.0
+      echo "Using HIPSYCL_PATH=${HIPSYCL_PATH}"
+      MAKE_OPTS=" -DSYCL_RUNTIME=HIPSYCL"
+      MAKE_OPTS+=" -DNUM_TD_PER_THREAD=16"
+      MAKE_OPTS+=" -DHIPSYCL_INSTALL_DIR=$HIPSYCL_PATH"
+      MAKE_OPTS+=" -DHIPSYCL_PLATFORM=cpu"
+      MAKE_OPTS+=" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++"
+      ;;
+    computecpp-2.1.1)
+      module load gcc/8.2.0
+      loadOneAPI /lustre/projects/bristol/modules/intel/oneapi/setvars.sh # for the Intel OpenCL libs
+      module load computecpp/2.1.1
+      module load cmake/3.18.3
+      COMPUTECPP_PATH="$(realpath "$(dirname "$(which compute++)")"/..)"
+      INTEL_OCL_LIB_PATH="$(realpath "$(dirname "$(which icc)")"/../..)/lib/libOpenCL.so.1"
+      echo "Using COMPUTECPP_PATH=${COMPUTECPP_PATH}"
+      echo "Using INTEL_OCL_LIB_PATH=${INTEL_OCL_LIB_PATH}"
+      MAKE_OPTS=" -DSYCL_RUNTIME=COMPUTECPP"
+      MAKE_OPTS+=" -DNUM_TD_PER_THREAD=2"
+      MAKE_OPTS+=" -DComputeCpp_DIR=$COMPUTECPP_PATH"
+      MAKE_OPTS+=" -DOpenCL_LIBRARY=${INTEL_OCL_LIB_PATH}"
+      MAKE_OPTS+=" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++"
       ;;
     *)
       echo
@@ -34,8 +75,6 @@ setup_env() {
       exit 1
       ;;
   esac
-
-  MAKE_OPTS+=' WGSIZE=512'
 }
 export -f setup_env
 
@@ -44,9 +83,9 @@ SCRIPT_DIR="$(realpath "$(dirname "$script")")"
 PLATFORM_DIR="$(realpath "$(dirname "$script")")"
 export SCRIPT_DIR PLATFORM_DIR
 
-export COMPILERS="cce-10.0 gcc-9.3 gcc-10.2 intel-2019"
+export COMPILERS="cce-10.0 gcc-9.3 gcc-10.2 intel-2019 oneapi-2021.1-beta10 hipsycl-46bc9bd computecpp-2.1.1"
 export DEFAULT_COMPILER="cce-10.0"
-export MODELS="omp kokkos"
+export MODELS="omp kokkos sycl"
 export DEFAULT_MODEL="omp"
 export PLATFORM="rome-isambard"
 
